14ab16298e089a1814ddc6fa5caafc6269cf5626
tyli091 <493642155@qq.com>
2019-07-23 11:48:45 +0800

    for andrioid 6

--- src/Android.mk
+++ src/Android.mk
@@ -1,4 +1,4 @@
-#Copyright (C) 2012 The Android Open Source Project
+# Copyright (C) 2012 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,12 +20,9 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := audio.sklrm.default
-
-
 LOCAL_MODULE_RELATIVE_PATH := hw
-
 LOCAL_SRC_FILES := \
-	audio_hw_sklrm.c decoder.c
+	audio_hw_add.c decoder.c
 LOCAL_C_INCLUDES += \
 	external/tinyalsa/include \
 	$(call include-path-for, audio-utils) 
